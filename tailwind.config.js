cdbe52a1e9851e84c048ba132f19bc0b15504307
nutza <www.nutzazaza@gmail.com>
2024-09-26 17:00:32 +0700

    second

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}","./index2.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
